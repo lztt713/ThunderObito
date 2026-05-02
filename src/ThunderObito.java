b511c6a67cf489354169d54e5f94813e7ef582c9
lztt713 <81623555@qq.com>
2026-05-02 20:39:18 +0800

    first try

---
+++ src/ThunderObito.java
@@ -0,0 +1,17 @@
+public class ThunderObito extends GameEngine
+{
+    public static  void main(String[] args)
+    {
+        createGame(new ThunderObito(),10);
+    }
+
+
+    public void update(double dt) {
+
+    }
+
+
+    public void paintComponent() {
+
+    }
+}
